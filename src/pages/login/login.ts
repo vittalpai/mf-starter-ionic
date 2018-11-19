7015e28477b1e4d1317097c0e7ec1fe1c4029468
Vittal Pai <vittalpai@ymail.com>
2018-11-19 16:36:05 +0530

    Add Login Screen

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-login',
+  templateUrl: 'login.html'
 })
-export class HomePage {
+export class LoginPage {
 
   constructor(public navCtrl: NavController) {
-
   }
-
+  
 }
