7441886a2cad27b12e6d35ad1e6a7f5191d0de9e
Vittal Pai <vittalpai@ymail.com>
2018-11-19 17:29:15 +0530

    Add MF Functionality and login backend code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, Renderer, ViewChild } from '@angular/core';
+import { Platform, App, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 import { LoginPage } from '../pages/login/login';
 @Component({
@@ -10,13 +11,44 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  private UserLoginChallengeHandler: any
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, renderer: Renderer, public appCtrl: App, public alertCtrl: AlertController, private _LoginPage: LoginPage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    // register mfp init function after plugin loaded
+    renderer.listenGlobal('document', 'mfpjsloaded', () => {
+      console.log('--> MobileFirst API plugin init complete');
+      this.MFPInitComplete();
+    });
+  }
+
+  // MFP Init complete function
+  MFPInitComplete() {
+    console.log('--> MFPInitComplete function called');
+    this.registerChallengeHandler();  // register a ChallengeHandler callback for UserLogin security check
+  }
+
+  registerChallengeHandler() {
+    this.UserLoginChallengeHandler = WL.Client.createSecurityCheckChallengeHandler("UserLogin");
+    this.UserLoginChallengeHandler.handleChallenge = ((challenge: any) => {
+      console.log('--> UserLoginChallengeHandler.handleChallenge called');
+      this.displayLoginChallenge(challenge);
+    });
+  }
+
+  displayLoginChallenge(response) {
+    if (response.errorMsg) {
+      var msg = response.errorMsg + ' <br> Remaining attempts: ' + response.remainingAttempts;
+      console.log('--> displayLoginChallenge ERROR: ' + msg);
+      this._LoginPage.updateResult(msg);
+    } else {
+      this._LoginPage.updateResult('Invalid Credentials');
+    }
   }
 }
-
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home'
 
 @Component({
   selector: 'page-login',
@@ -7,7 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
+  private securityCheck = "Userlogin"
+  public result: string;
+
   constructor(public navCtrl: NavController) {
+  }
+
+  login(credentials : Object) {
+    WLAuthorizationManager.login(this.securityCheck, credentials).then(
+      () => {
+        console.log('-->  login(): Success ');
+        this.navCtrl.push(HomePage);
+      }, (error) => {
+        console.log('-->  login(): Failure ', error);
+        this.updateResult('Invalid Credentials, Try after sometime.');
+      }
+    )
+  }
+
+  public updateResult(msg: string) {
+    this.result = msg;
   }
   
 }
